e31de9c6fd0aa1299dee13a5a982608376f39bc3
wes-mitchell <wmdrums@gmail.com>
2022-05-19 14:21:39 -0500

    moose can ask a question with moose asks funtion

--- Program.cs
+++ Program.cs
@@ -5,6 +5,27 @@ Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
 Console.WriteLine("--------------------------------------------");
 Console.WriteLine();
 
+bool MooseAsks(string question)
+{
+    Console.WriteLine($"{question} (Y/N): ");
+    string answer = Console.ReadLine().ToLower();
+
+    while (answer != "y" && answer != "n")
+    {
+        Console.Write($"{question} (Y/N): ");
+        answer = Console.ReadLine().ToLower();
+    }
+
+    if (answer == "y")
+    {
+        return true;
+    }
+    else 
+    {
+        return false;
+    }
+}
+
 void MooseSays(string message)
 {
       Console.WriteLine($@"
@@ -37,6 +58,10 @@ void MooseSays(string message)
     ");
 }
 
+// Let the moose speak!
 MooseSays("H I, I'M  E N T H U S I A S T I C !");
 MooseSays("I really am enthusiastic");
 
+// As a question
+bool isTrue = MooseAsks("Is Canada real?");
+Console.WriteLine(isTrue);
