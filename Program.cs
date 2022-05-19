d4124b044c8929cd37d260565696e961d930b4bc
wes-mitchell <wmdrums@gmail.com>
2022-05-19 14:41:16 -0500

    creates questions as a function

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 
+Main();
 
+void Main()
+{
 Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
 Console.WriteLine("--------------------------------------------");
 Console.WriteLine();
@@ -62,13 +65,58 @@ void MooseSays(string message)
 MooseSays("H I, I'M  E N T H U S I A S T I C !");
 MooseSays("I really am enthusiastic");
 
-// As a question
-bool isTrue = MooseAsks("Is Canada real?");
-if (isTrue) 
+void CanadaQuestion()
 {
-    MooseSays("Really? It seems very unlikely.");
-} 
-else
+    bool isTrue = MooseAsks("Is Canada Real?");
+    if (isTrue)
+    {
+        MooseSays("Really? It seems very unlikely.");
+    }
+    else 
+    {
+        MooseSays("I  K N E W  I T !!!");
+    }
+}
+
+void EatsGerbilsQuestion()
 {
-    MooseSays(" I  K N E W  I T !!!");
+    bool eatsGerbils = MooseAsks("Do you eat gerbils?");
+    if (eatsGerbils)
+    {
+        MooseSays("Yay!");
+    }
+    else
+    {
+        MooseSays("You should try it!");
+    }
+}
+
+void KoolAidManQuestion()
+{
+    bool knowsKoolAidMan = MooseAsks("Do you know the Kool Aid Man?");
+    if (knowsKoolAidMan)
+    {
+        MooseSays("O H  Y E A !");
+    }
+    else
+    {
+        MooseSays("Well, dang ol boom.");
+    }
+}
+
+void SecretQuestion()
+{
+    bool wantsSecret = MooseAsks("Do you want to know a secret?");
+    if (wantsSecret)
+    {
+        MooseSays("ME TOO!!!! I love secrets...tell me one!");
+    }
+    else
+    {
+        MooseSays("Oh, no...secrets are the best, I love to share them!");
+    }
+}
+
+KoolAidManQuestion();
+EatsGerbilsQuestion();
 }
